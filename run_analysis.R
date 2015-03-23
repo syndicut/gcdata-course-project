d2dfd2e4c659b3185db4b171be0faf610348f05e
Rashit Azizbaev <syndicut@yandex-team.ru>
2015-03-23 08:09:13 +0300

    Adde dplyr package

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 datasetURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 datasetFile <- "UCI HAR Dataset.zip"
 datasetDir <- "UCI HAR Dataset"
